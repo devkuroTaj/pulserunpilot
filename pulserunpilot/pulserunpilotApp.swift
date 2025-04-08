b602019fc9d54a55807783d646b5bc6d755895d5
devkuroTaj <devkuro96@gmail.com>
2025-04-08 18:17:13 +0900

    Initial Commit

---
+++ pulserunpilot/pulserunpilotApp.swift
@@ -0,0 +1,17 @@
+//
+//  pulserunpilotApp.swift
+//  pulserunpilot
+//
+//  Created by 장진성 on 4/8/25.
+//
+
+import SwiftUI
+
+@main
+struct pulserunpilotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ pulserunpilotTests/pulserunpilotTests.swift
@@ -0,0 +1,16 @@
+//
+//  pulserunpilotTests.swift
+//  pulserunpilotTests
+//
+//  Created by 장진성 on 4/8/25.
+//
+
+import Testing
+
+struct pulserunpilotTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
